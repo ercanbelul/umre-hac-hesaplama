2aa44ac56977d9a972a7e0539df971fd9bba674a
ercanbelul <github-mail-adresiniz@example.com>
2025-07-22 19:52:23 +0300

    webDir düzeltmesi

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ercanbelul.umrehesaplayici',
+  appName: 'umreHesaplayici',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
